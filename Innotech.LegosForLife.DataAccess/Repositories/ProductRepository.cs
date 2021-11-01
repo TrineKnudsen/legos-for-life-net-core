d075cff03399de67cca3aa2c9e6b6769f85f173a
TrineKnudsen <trinesaknudsen@hotmail.com>
2021-11-01 17:33:01 +0100

    Entity and repos

---
+++ Innotech.LegosForLife.DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using InnoTech.LegosForLife.Core.Models;
+using InnoTech.LegosForLife.Domain.IRepositories;
+
+namespace Innotech.LegosForLife.DataAccess.Repositories
+{
+    public class ProductRepository: IProductRepository
+    {
+        public List<Product> FindAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Onion-Infratructure-Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Onion_Infratructure_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
