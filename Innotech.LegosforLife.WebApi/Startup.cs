6b5c9b64b38c31409225e5082103acb54c24839c
TrineKnudsen <trinesaknudsen@hotmail.com>
2021-11-09 21:41:12 +0100

    dbSeeder

--- Innotech.LegosforLife.WebApi/Startup.cs
+++ Innotech.LegosforLife.WebApi/Startup.cs
@@ -1,5 +1,11 @@
+using InnoTech.LegosForLife.Core.IServices;
+using InnoTech.LegosForLife.DataAccess;
+using InnoTech.LegosForLife.DataAccess.Repositories;
+using InnoTech.LegosForLife.Domain.IRepositories;
+using InnoTech.LegosForLife.Domain.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,16 +30,29 @@ namespace InnoTech.LegosForLife.WebApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Innotech.LegosforLife.WebApi", Version = "v1" });
             });
+            
+            //Setting up dependency injection
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+            
+            //Setting up DB info
+            services.AddDbContext<MainDbContext>(
+                options =>
+                {
+                    options.UseSqlite("Data Source =main.db");
+                });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, MainDbContext context)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Innotech.LegosforLife.WebApi v1"));
+                new DbSeeder(context).SeedDevelopment();
             }
 
             app.UseHttpsRedirection();
